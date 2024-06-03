740c97007c578b153f548e4cab06e90ae752ee09
Artem <artem.yaremenko.1992@gmail.com>
2024-06-04 01:59:59 +0300

    Gryffindor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Gryffindor harryPotter = new Gryffindor("Harry", "Potter", 60, 40, 60, 70, 80);
+        Gryffindor hermioneGranger = new Gryffindor("Hermione", "Granger", 50, 40, 60, 50, 60);
+        Gryffindor ronWeasley = new Gryffindor("Ron", "Weasley", 50, 20, 50, 80, 60);
 
+        ronWeasley.comparingGryffindor(hermioneGranger);
+        Gryffindor.displayStudentInfo(harryPotter);
     }
 }
